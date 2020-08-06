0daf2bef480c41dbb79f005cf911e4776ba5654a
juberbadi <juberbadi052@gmail.com>
2020-08-06 22:40:18 +0530

    07ConditionalSignout

---
+++ 02Basics/07ConditionalSignout.js
@@ -0,0 +1,13 @@
+
+var authenticated = true;
+
+// if(authenticated)
+// {
+//     console.log("Show Signout button");
+// }
+// else
+// {
+//     console.log("Show login option");
+// }
+
+authenticated ? console.log("Logout Button") : console.log("Login Button");
